5e4344e8b928def1d489d20879067dfaa06a6ae9
Abdalrhman8000 <abdo.eslam2021@gmail.com>
2022-09-21 12:22:51 +0200

    UPD Some Files And ADD some futures

---
+++ src/components/LoadApp.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="load-animate">
+        <div class="load">
+            <div class="img">
+                <img src="./assets/Load/2.webp" alt="">
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss">
+    
+</style>
